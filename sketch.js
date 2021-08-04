feb9940a5485f406c9c4e7e70d0989815703e048
Heni2 <88445021+Heni2@users.noreply.github.com>
2021-08-04 10:35:18 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var ball;
+function setup() {
+  createCanvas(400,400);
+  ball = createSprite(200,200,20,20)
+}
+
+function draw() 
+{
+  background(30);
+ drawSprites();
+ if(keyIsDown(RIGHT_ARROW)) {
+     ball.x += 3
+ }
+ if(keyIsDown(LEFT_ARROW)) {
+  ball.x -= 3
+}
+if(keyIsDown(UP_ARROW)) {
+  ball.y -= 3
+}
+if(keyIsDown(DOWN_ARROW)) {
+  ball.y += 3
+}
+}
+
+
+
